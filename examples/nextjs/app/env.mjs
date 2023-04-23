346be9de725d8b3de9ed8357b2fcec38e78a4eef
Julius Marminge <julius0216@outlook.com>
2023-04-23 23:33:19 +0200

    rm unused

--- examples/nextjs/app/env.mjs
+++ examples/nextjs/app/env.mjs
@@ -7,11 +7,9 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_GREETING: z.string(),
-    NEXT_PUBLIC_FOO: z.string().transform((s) => Number(s)),
   },
   runtimeEnv: {
     SECRET: process.env.SECRET,
     NEXT_PUBLIC_GREETING: process.env.NEXT_PUBLIC_GREETING,
-    NEXT_PUBLIC_FOO: process.env.NEXT_PUBLIC_FOO,
   },
 });
