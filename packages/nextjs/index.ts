f0f702fdfecc2dc2631dc791899efce8eaae1cd5
Chung Wei <chungwei@whiteroom.work>
2023-04-26 11:37:26 +0800

    fix: NextJS options type

--- packages/nextjs/index.ts
+++ packages/nextjs/index.ts
@@ -1,67 +1,29 @@
-import type { z, ZodType } from "zod";
-import { createEnv as createEnvCore, type ErrorMessage } from "../core";
+import { type ZodType } from "zod";
+
+import { createEnv as createEnvCore, type StrictOptions } from "../core";
+
+type ClientPrefix = "NEXT_PUBLIC_";
+
+interface Options<
+  TServer extends Record<string, ZodType>,
+  TClient extends Record<`${ClientPrefix}${string}`, ZodType>
+> extends Omit<
+    StrictOptions<ClientPrefix, TServer, TClient>,
+    "runtimeEnvStrict" | "runtimeEnv" | "clientPrefix"
+  > {
+  /**
+   * Manual destruction of `process.env`. Required for Next.js.
+   */
+  runtimeEnv: StrictOptions<ClientPrefix, TServer, TClient>["runtimeEnvStrict"];
+}
 
 export function createEnv<
   TServer extends Record<string, ZodType>,
-  TClient extends Record<string, ZodType>
->(opts: {
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app isn't
-   * built with invalid env vars.
-   */
-  server: TServer;
-
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app isn't
-   * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
-   */
-  client: {
-    [TKey in keyof TClient]: TKey extends `NEXT_PUBLIC_${string}`
-      ? TClient[TKey]
-      : ErrorMessage<`${TKey extends string
-          ? TKey
-          : never} is not prefixed with NEXT_PUBLIC_.`>;
-  };
-
-  /**
-   * Manual destruction of `process.env`. Required for Next.js.
-   * @default process.env
-   */
-  runtimeEnv: Record<
-    | {
-        [TKey in keyof TClient]: TKey extends `NEXT_PUBLIC_${string}`
-          ? TKey
-          : never;
-      }[keyof TClient]
-    | keyof TServer,
-    string | undefined
-  >;
-
-  /**
-   * Whether to skip validation of environment variables.
-   * @default !!process.env.SKIP_ENV_VALIDATION && process.env.SKIP_ENV_VALIDATION !== "false" && process.env.SKIP_ENV_VALIDATION !== "0"
-   */
-  skipValidation?: boolean;
-
-  /**
-   * Called when validation fails. By default the error is logged,
-   * and an error is thrown telling what environment variables are invalid.
-   * Function must throw an error at the end.
-   */
-  onValidationError?: (error: z.ZodError) => never;
-
-  /**
-   * Called when a server-side environment variable is accessed on the client.
-   * By default an error is thrown.
-   */
-  onInvalidAccess?: (variable: string) => never;
-}) {
-  return createEnvCore({
+  TClient extends Record<`${ClientPrefix}${string}`, ZodType>
+>({ runtimeEnv, ...opts }: Options<TServer, TClient>) {
+  return createEnvCore<ClientPrefix, TServer, TClient>({
     ...opts,
-    // @ts-expect-error TODO: Fix this
-    client: opts.client,
-    runtimeEnvStrict: opts.runtimeEnv,
-    // @ts-expect-error TODO: Fix this
     clientPrefix: "NEXT_PUBLIC_",
+    runtimeEnvStrict: runtimeEnv,
   });
 }
