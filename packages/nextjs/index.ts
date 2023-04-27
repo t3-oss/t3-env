1de8c89f7afdc26dc24e41447d9a3de55c8a8f66
Julius Marminge <julius0216@outlook.com>
2023-04-28 00:11:54 +0200

    Update packages/nextjs/index.ts

--- packages/nextjs/index.ts
+++ packages/nextjs/index.ts
@@ -6,11 +6,8 @@ const CLIENT_PREFIX = "NEXT_PUBLIC_" as const;
 type ClientPrefix = typeof CLIENT_PREFIX;
 
 interface Options<
-  TServer extends Record<string, ZodType> = NonNullable<unknown>,
-  TClient extends Record<
-    `${ClientPrefix}${string}`,
-    ZodType
-  > = NonNullable<unknown>
+  TServer extends Record<string, ZodType>,
+  TClient extends Record<`${ClientPrefix}${string}`, ZodType>
 > extends Omit<
     StrictOptions<ClientPrefix, TServer, TClient>,
     "runtimeEnvStrict" | "runtimeEnv" | "clientPrefix"
