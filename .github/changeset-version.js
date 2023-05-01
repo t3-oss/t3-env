6acdeb8c78ccb22aba48957169c166e977087b91
juliusmarminge <julius0216@outlook.com>
2023-05-01 11:10:19 +0200

    pnpm isntall?

--- .github/changeset-version.js
+++ .github/changeset-version.js
@@ -8,4 +8,4 @@ import { exec } from "child_process";
 // This is a workaround until this is handled automatically by `changeset version`.
 // See https://github.com/changesets/changesets/issues/421.
 exec("npx changeset version");
-exec("npm install");
+exec("pnpm install");
