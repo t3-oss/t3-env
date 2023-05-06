316128b18a443f22cb50e863432eae03f5672fb8
juliusmarminge <julius0216@outlook.com>
2023-05-06 15:59:01 +0200

    fixy

--- packages/nuxt/index.ts
+++ packages/nuxt/index.ts
@@ -1,51 +1,24 @@
-import type { ZodError, ZodType } from "zod";
-import { createEnv as createEnvCore, type ErrorMessage } from "../core";
+import type { ZodType } from "zod";
+import { createEnv as createEnvCore, StrictOptions } from "../core";
+
+const CLIENT_PREFIX = "NUXT_PUBLIC_" as const;
+type ClientPrefix = typeof CLIENT_PREFIX;
+
+type Options<
+  TServer extends Record<string, ZodType>,
+  TClient extends Record<`${ClientPrefix}${string}`, ZodType>
+> = Omit<
+  StrictOptions<ClientPrefix, TServer, TClient>,
+  "runtimeEnvStrict" | "runtimeEnv" | "clientPrefix"
+>;
 
 export function createEnv<
-  TServer extends Record<string, ZodType>,
-  TClient extends Record<string, ZodType>
->(opts: {
-  /**
-   * Specify your server-side environment variables schema here. This way you can ensure the app isn't
-   * built with invalid env vars.
-   */
-  server: TServer;
-
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app isn't
-   * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
-   */
-  client: {
-    [TKey in keyof TClient]: TKey extends `NUXT_PUBLIC_${string}`
-      ? TClient[TKey]
-      : ErrorMessage<`${TKey extends string
-          ? TKey
-          : never} is not prefixed with NUXT_PUBLIC_.`>;
-  };
-
-  /**
-   * Whether to skip validation of environment variables.
-   * @default !!process.env.SKIP_ENV_VALIDATION && process.env.SKIP_ENV_VALIDATION !== "false" && process.env.SKIP_ENV_VALIDATION !== "0"
-   */
-  skipValidation?: boolean;
-
-  /**
-   * Called when validation fails. By default the error is logged,
-   * and an error is thrown telling what environment variables are invalid.
-   * Function must throw an error at the end.
-   */
-  onValidationError?: (error: ZodError) => never;
-
-  /**
-   * Called when a server-side environment variable is accessed on the client.
-   * By default an error is thrown.
-   */
-  onInvalidAccess?: (variable: string) => never;
-}) {
-  return createEnvCore({
+  TServer extends Record<string, ZodType> = NonNullable<unknown>,
+  TClient extends Record<string, ZodType> = NonNullable<unknown>
+>(opts: Options<TServer, TClient>) {
+  return createEnvCore<ClientPrefix, TServer, TClient>({
     ...opts,
-    // @ts-expect-error TODO: Fix this
-    client: opts.client,
-    clientPrefix: "NUXT_PUBLIC_",
+    clientPrefix: CLIENT_PREFIX,
+    runtimeEnv: process.env,
   });
 }
