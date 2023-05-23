fdea97bb47a055d887d7a2609dd67116e6600086
parnavh <parnav100sach@gmail.com>
2023-05-23 13:33:13 +0530

    changes to next/nuxt packages

--- packages/nextjs/index.ts
+++ packages/nextjs/index.ts
@@ -1,6 +1,10 @@
 import { type ZodType } from "zod";
 
-import { createEnv as createEnvCore, type StrictOptions } from "../core";
+import {
+  createEnv as createEnvCore,
+  ServerClientOptions,
+  type StrictOptions,
+} from "../core";
 
 const CLIENT_PREFIX = "NEXT_PUBLIC_" as const;
 type ClientPrefix = typeof CLIENT_PREFIX;
@@ -9,7 +13,8 @@ interface Options<
   TServer extends Record<string, ZodType>,
   TClient extends Record<`${ClientPrefix}${string}`, ZodType>
 > extends Omit<
-    StrictOptions<ClientPrefix, TServer, TClient>,
+    StrictOptions<ClientPrefix, TServer, TClient> &
+      ServerClientOptions<ClientPrefix, TServer, TClient>,
     "runtimeEnvStrict" | "runtimeEnv" | "clientPrefix"
   > {
   /**
@@ -25,8 +30,31 @@ export function createEnv<
     ZodType
   > = NonNullable<unknown>
 >({ runtimeEnv, ...opts }: Options<TServer, TClient>) {
+  const client =
+    typeof opts.client === "object"
+      ? opts.client
+      : ({} as {
+          [TKey in keyof TClient]: TKey extends `NEXT_PUBLIC_${string}`
+            ? TClient[TKey]
+            : `${TKey extends string
+                ? TKey
+                : never} is not prefixed with NEXT_PUBLIC_.`;
+        });
+  const server =
+    typeof opts.server === "object"
+      ? opts.server
+      : ({} as {
+          [TKey in keyof TServer]: TKey extends `NEXT_PUBLIC_${string}`
+            ? `${TKey extends `NEXT_PUBLIC_${string}`
+                ? TKey
+                : never} should not prefixed with NEXT_PUBLIC_.`
+            : TServer[TKey];
+        });
+
   return createEnvCore<ClientPrefix, TServer, TClient>({
     ...opts,
+    client,
+    server,
     clientPrefix: CLIENT_PREFIX,
     runtimeEnvStrict: runtimeEnv,
   });
