473e789e1376a4637b3cb8f7b0abeb3d81d1fba0
SameerJadav <sameerjadav010@gmail.com>
2023-07-17 17:58:30 +0530

    fix: resolve incorrect fallback

--- docs/src/components/main-nav.tsx
+++ docs/src/components/main-nav.tsx
@@ -17,7 +17,7 @@ export interface NavItem {
 }
 
 export function MainNav(props: { items: NavItem[] }) {
-  const segment = useSelectedLayoutSegment() ?? "/";
+  const segment = useSelectedLayoutSegment();
 
   return (
     <div className="flex gap-6 md:gap-10">
@@ -32,7 +32,9 @@ export function MainNav(props: { items: NavItem[] }) {
                   className={cn(
                     "text-foreground/60 text-sm font-medium transition-colors hover:text-foreground/80 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background rounded-sm",
                     item.disabled && "cursor-not-allowed opacity-80",
-                    item.href.startsWith(`/${segment}`) && "text-foreground"
+                    segment &&
+                      item.href.startsWith(`/${segment}`) &&
+                      "text-foreground"
                   )}
                 >
                   {item.title}
