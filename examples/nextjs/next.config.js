fe05a3cc26318145ac6119ad2d816875130e74b8
Julius Marminge <julius0216@outlook.com>
2024-02-08 11:25:20 +0100

    chore: use jiti to load ts files in next (#188)

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,10 +1,10 @@
-import "./app/env.js";
+import createJiti from "jiti";
+const jiti = createJiti(new URL(import.meta.url).pathname);
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./app/env");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+export default {
+  eslint: { ignoreDuringBuilds: true },
 };
-
-export default nextConfig;
