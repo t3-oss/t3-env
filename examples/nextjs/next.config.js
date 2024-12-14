b7e570aeeca27d14a84759e7143c83bb0cd5f53b
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-12-14 20:00:10 +0000

    fix: correct outdated documentation using deprecated jiti functions in nextjs documentation and example

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,8 +1,8 @@
-import createJiti from "jiti";
+import { createJiti } from "jiti";
 const jiti = createJiti(new URL(import.meta.url).pathname);
 
 // Import env here to validate during build. Using jiti we can import .ts files :)
-jiti("./app/env");
+await jiti.import("./app/env");
 
 /** @type {import('next').NextConfig} */
 export default {
