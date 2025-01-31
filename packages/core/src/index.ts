3c4c5080399a3776477b60fa5af3ae744aaab331
Ben Durrant <ben.durrant@marketdojo.com>
2025-01-31 01:32:51 +0000

    no need to make a Set

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -324,7 +324,7 @@ const makeInternalProp = <T>(name: string) => {
   };
 };
 
-const serverKeysProp = makeInternalProp<Set<string>>("serverKeys");
+const serverKeysProp = makeInternalProp<string[]>("serverKeys");
 
 export function createEnv<
   TPrefix extends TPrefixFormat,
@@ -380,8 +380,6 @@ export function createEnv<
 
   ensureSynchronous(parsed, "Validation must be synchronous");
 
-  const serverKeys = new Set(Object.keys(_server));
-
   const onValidationError =
     opts.onValidationError ??
     ((issues) => {
@@ -413,6 +411,8 @@ export function createEnv<
   };
 
   // a map of keys to the preset we got them from
+  const serverKeys = Object.keys(_server);
+
   const presetsByKey: Record<string, Record<string, unknown> | undefined> = {};
 
   const extendedObj = (opts.extends ?? []).reduce((acc, curr) => {
@@ -420,10 +420,10 @@ export function createEnv<
       presetsByKey[key] = curr;
       acc[key] = curr[key];
     }
-    const serverKeys = serverKeysProp.getValue(curr);
-    if (serverKeys) {
+    const presetServerKeys = serverKeysProp.getValue(curr);
+    if (presetServerKeys) {
       // these are keys that the proxy should handle but won't be exposed on the client
-      for (const key of serverKeys) {
+      for (const key of presetServerKeys) {
         presetsByKey[key] = curr;
       }
     }
