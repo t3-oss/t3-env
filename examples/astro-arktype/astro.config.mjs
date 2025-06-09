490e0143279cef473dc90a7c77060862983bb857
juliusmarminge <jmarminge@gmail.com>
2025-06-09 17:31:59 +0200

    add arktype presets and tests

---
+++ examples/astro-arktype/astro.config.mjs
@@ -0,0 +1,7 @@
+import solidJs from "@astrojs/solid-js";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solidJs()],
+});
