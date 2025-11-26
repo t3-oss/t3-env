514c0aa9404a78d1929f21e1205245d0a8fea2a6
Julius <julius0216@outlook.com>
2025-11-25 22:14:11 -0800

    rm

--- examples/nuxt/app.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import { env } from "~~/env";
-</script>
-<template>
-  <div>Client says {{ env.NUXT_PUBLIC_GREETING }}!</div>
-  <a href="/api/hello">See what the server has to say!</a>
-</template>
--- examples/nuxt/nuxt.config.ts
+++
@@ -1,7 +0,0 @@
-// oxlint-disable-next-line no-unassigned-import - side effect import to validate during build
-import "./env";
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  // ...
-});
