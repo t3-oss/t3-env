235ba7f3c2965b755b2c74fc4413674f62975aa0
Aziz Becha <aziz07becha@gmail.com>
2025-12-14 01:41:07 +0100

    configure husky's pre-commit to format and lint before pushing

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -2,5 +2,6 @@
 import { env } from "~/env";
 
 export default function HomePage() {
-  return <div>Client says {env.NEXT_PUBLIC_GREETING}!</div>;
+        return 
+        <div>Client says {env.NEXT_PUBLIC_GREETING}!</div>;
 }
