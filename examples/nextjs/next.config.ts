0b1f95526504a028851322cc0cbcdf1dee9f3d8d
Julius <julius0216@outlook.com>
2025-12-14 20:39:36 -0800

    use ts config in our example

---
+++ examples/nextjs/next.config.ts
@@ -0,0 +1,8 @@
+// Import env here to validate during build
+import { env as _env } from "./app/env";
+
+import type { NextConfig } from "next";
+
+export default {
+  // ...
+} satisfies NextConfig;
